d1fb4f7a17aba656fa8f4463720fd64e1f7a170d
Jack <jdcorley@gmail.com>
2018-12-22 12:43:22 -0500

    sinatra-contrib and json responses

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
+require 'sinatra-contrib'
 
 class ConferenceApp < Sinatra::Base
 
   get '/' do 
     'It Works!'
+  end
+
+  get '/hello.json' do 
+    message = { success: true, message: 'hello' }
+    json message
   end
 
 end
