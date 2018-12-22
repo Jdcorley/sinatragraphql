026babef6839b2c1e8fea78ca093887627128732
Jack <jdcorley@gmail.com>
2018-12-22 13:05:24 -0500

    fix json

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
-<<<<<<< HEAD
-=======
 require 'sinatra/json'
->>>>>>> add-json-responses
 
 class ConferenceApp < Sinatra::Base
 
   get '/' do 
     'It Works!'
+  end
+
+  get '/hello.json' do 
+    message = { success: true, message: 'hello' }
+    json message
   end
 
 end
